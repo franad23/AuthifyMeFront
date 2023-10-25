04da02d5cb6ba877c7916acefb5407300b59f202
Franco Diaz <dfrancoandres@gmail.com>
2023-10-25 20:44:49 -0300

    se agrega conexion de login a backend

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,41 @@
 import logo from '../../assets/logo.png'
 import { Link } from "react-router-dom";
+import { FormEvent } from 'react';
+import { Toaster, toast } from 'sonner'
 import './loginpage.css'
 
 //Components 
 import InputShared from '../../Components/Shared/InputShared/InputShared';
 import { ButtonForm } from '../../Components/Shared/Buttons/Buttons';
 
+//API
+import { loginMainUser } from '../../api/auth.api';
+
 function LoginPage() {
+
+  const handleLoginMainUser = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const email = (e.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (e.currentTarget.elements[1] as HTMLInputElement).value;
+    try {
+      const registerApiResponse = await loginMainUser(email, password);
+      console.log(registerApiResponse);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message)
+    }
+  }
+
+
   return (
     <div className='registerPageMainContainer'>
-
+      <Toaster position="top-right" />
       <div className='registerContainer'>
         <div className='logoContainer'>
           <img src={logo} alt="logo" className='logo'/>
           <h1>AuthifyMe</h1>
         </div>
-        <form className='formRegister'>
+        <form className='formRegister' onSubmit={handleLoginMainUser}>
           <InputShared
             name='email'
             placeholder='Ingrese su Email'
--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -3,3 +3,6 @@ import axios from '../libs/axios.libs'
 export const registerMainUser = async (username:string, email:string, password:string) => {
   return axios.post("/register" , ({username, email, password}))
 }
+export const loginMainUser = async (email:string, password:string) => {
+  return axios.post("/login" , ({email, password}))
+}
